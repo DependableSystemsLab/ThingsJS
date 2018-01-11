af411c72987b69602f0f793249945a8cec4fdc1a
Atif Mahmud <atifmahmud95@gmail.com>
2018-01-11 14:42:03 -0800

    Changed position of 'Create New Folder' button

--- util/dashboard/db.js
+++ util/dashboard/db.js
@@ -88,6 +88,10 @@ DashboardDatabase.prototype.deleteCode = function(name){
 			})
 	});
 };
+DashboardDatabase.prototype.createNewFolder = function(){
+	
+};
+
 DashboardDatabase.prototype.getCode = function(name){
 	return this.db.collection("smartjs-raw-code").findOne({ _id: name });
 };
