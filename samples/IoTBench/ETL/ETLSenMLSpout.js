c3b5baa754c9db3a2af452b29aeb9ee40e5c0d7e
Julien Gascon-Samson <julien.gascon-samson@ece.ubc.ca>
2018-04-14 14:30:25 -0700

    Started work on porting IoTBench samples from RIOTBench (ETL)... Coded SenMLParse and a spout which feeds data from the TAXI DB

---
+++ samples/IoTBench/ETL/ETLSenMLSpout.js
@@ -0,0 +1,61 @@
+/* 
+ * Publishes SenML data over pub sub
+ */
+
+//var things = require('things-js');
+var things = require('../../../lib/things.js')
+
+/* configurable variables */
+var pubsub_url = 'mqtt://localhost';
+var pubsub_topic = 'thingsjs/IoTBench/ETL/SenMLSpout';
+var publish_interval = 1000;
+
+var pubsub = new things.Pubsub('ETLSenMLSpout', pubsub_url);
+
+/* SenML Lines */
+var lines = [];
+var currentLine = 0;
+
+function startSpout() {
+
+	
+	var lineReader = require('readline').createInterface({
+		input: require('fs').createReadStream('TAXI_sample_data_senml.csv')
+	});
+
+	lineReader.on('line', function (line) {
+		//console.log('Line from file:', line);
+		lines.push(line);
+	});
+
+	lineReader.on('close', function() {
+		// File ended
+		console.log('Done reading file... Starting to publish');
+		setInterval(publishLine, publish_interval);
+	});
+	
+}
+
+/* Publish a SenML line */
+function publishLine() {
+	// For the moment, publish as a raw string rather than json-serializing
+	// since de-jsoning will be performed by the next 'bolt'
+	if (currentLine >= lines.length) {
+		/*
+		console.log("Done sending data.");
+		return;
+		*/
+	   currentLine = 0;
+	}
+	console.log("Publishing line " + currentLine);
+	pubsub.publish(pubsub_topic, lines[currentLine], true);
+	currentLine++;
+}
+
+/* Connect pubsub */
+pubsub.connect(function(){
+    console.log("Pubsub connected");
+    
+	// Start processing
+	startSpout();
+});
