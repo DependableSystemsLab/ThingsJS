DependableSystemsLab/ThingsJS

commit aa24b748230ce0d6c1c570f72323ec3b2a911897

- BugFix:     When an identifier refers to the enclosing named function     and there is a variable by the same name declared in the same scope as the named function,     the identifier points to the variable instead of pointing to the named function.

lib/core/Code.js
@@ -107,7 +107,8 @@ StaticScope.prototype.initRefs = function(node){
 }
 StaticScope.prototype.findRef = function(raw_name){
 	// if (raw_name in this.refs) return this.identifier()+'.refs.'+raw_name;
-	if (raw_name in this.refs) return { scope: this, identifier: this.identifier()+'.refs.'+raw_name }
+	if (raw_name in this.refs) return { scope: this, identifier: this.identifier()+'.refs.'+raw_name };
+	if (raw_name === this.func_name) return { scope: null, identifier: raw_name };	// If the enclosing function is named, the identifier refers to the function
 	if (this.parent !== null) return this.parent.findRef(raw_name);
 	else {
 		(WARNING && console.log(chalk.red('[WARNING] Could not find scope for ')+raw_name));

bin/things-js
@@ -116,7 +116,7 @@ program
   .option('-p, --port <port>', 'Port to Listen to')
   .option('-d, --debug', 'Set environment variable DEBUG to "true" and see debug output')
   .description('Start a Pubsub Server')
-  .action(function(port, options){
+  .action(function(options){
     process.env['DEBUG'] = options.debug ? "true": "false";
     var things = require('../lib/things.js');
     var server = new things.Pubsub.Server();

commit 400d03773c72bf2aa7adf1ff9e2f55242acea71e

- removed hard-coded IP - fixing bug introduced by aa24b748230ce0d6c1c570f72323ec3b2a911897

lib/core/Code.js
@@ -108,7 +108,7 @@ StaticScope.prototype.initRefs = function(node){
 StaticScope.prototype.findRef = function(raw_name){
 	// if (raw_name in this.refs) return this.identifier()+'.refs.'+raw_name;
 	if (raw_name in this.refs) return { scope: this, identifier: this.identifier()+'.refs.'+raw_name };
-	if (raw_name === this.func_name) return { scope: null, identifier: raw_name };	// If the enclosing function is named, the identifier refers to the function
+	if (raw_name === this.func_name) return { scope: null, identifier: raw_name+'.'+PROPERTY_PREFIX+'wrapped' };	// If the enclosing function is named, the identifier refers to the function
 	if (this.parent !== null) return this.parent.findRef(raw_name);
 	else {
 		(WARNING && console.log(chalk.red('[WARNING] Could not find scope for ')+raw_name));
@@ -1297,6 +1297,7 @@ Scope.prototype.addFunction = function(func, protects, uid){
 	wrapped[PROPERTY_PREFIX+'raw'] = func;
 
 	this.funcs[wrapped[PROPERTY_PREFIX+'uid']] = wrapped;
+	func[PROPERTY_PREFIX+'wrapped'] = wrapped;
 	
 	return wrapped;
 }

lib/util/MqttWsBridge.js
@@ -8,7 +8,7 @@ var helpers = require('../helpers.js');
 var DEBUG_MODE = process.env.DEBUG || true;
 
 var MQTT_DEFAULT = { url: 'mqtt://localhost' };
-var WSS_DEFAULT = { host: '192.168.0.73', port: 8000 };
+var WSS_DEFAULT = { host: '0.0.0.0', port: 8000 };
 
 /** This object acts as a bridge between a MQTT service and the browser,
  *    since browsers do not natively support MQTT protocol.