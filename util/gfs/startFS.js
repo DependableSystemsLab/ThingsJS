adf4f9f0e15452d1cff2f420e877e6ac910b7d92
jungkumseok <jungkumseok@gmail.com>
2018-10-01 12:32:23 -0700

    purged old GFS code

--- util/gfs/startFS.js
+++
@@ -1,8 +0,0 @@
-/*
- usage: node startFS.js
-*/
-FSServer = require('./FSServer.js');
-var mongoURL = 'mongodb://localhost:27017/things-filesystem'
-
-var server = new FSServer(mongoURL);
-server.start();
